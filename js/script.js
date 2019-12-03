3c7904da9775832d87c0257f207544115e04c502
Niklas <nik.meissner@web.de>
2019-12-04 00:40:02 +0100

    updated deep-linking

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var pos = 0;
 var pos1 = 0;
 var turn = 0;
 var nm_name = ['Niklas Meißner'];
-var nm_byline = ['Computer Scientist','Based in Stuttgart'];
+var nm_byline = ['Computer Scientist','Located in Stuttgart'];
 var speed = 200;
 var isClicked = false;
 var isScrolled = false;
@@ -99,6 +99,37 @@ $(document).ready(function() {
 	}
   });
 });
+
+// LinkedIn
+function linkedIn(){
+    if( (navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPod") != -1) || (navigator.platform.indexOf("iPad") != -1)) {
+         window.open("voyager://in/niklas-meißner", '_blank');
+    } else if(navigator.platform.indexOf("Android") != -1) {
+         window.open("intent://www.linkedin.com/in/niklas-meißner/#Intent;package=com.linkedin.android;scheme=https;end", '_blank');
+	} else {
+		window.open('https://www.linkedin.com/in/niklas-meißner', '_blank');
+	}
+}
+
+// Xing
+function xing(){
+    if( (navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPod") != -1) || (navigator.platform.indexOf("iPad") != -1)) {
+         window.open("xing://profile/Niklas_Meissner6", '_blank');
+    } else if(navigator.platform.indexOf("Android") != -1) {
+         window.open("intent://www.xing.com/profile/Niklas_Meissner6/#Intent;package=com.xing.android;scheme=https;end", '_blank');
+	} else {
+		window.open('https://www.xing.com/profile/Niklas_Meissner6', '_blank');
+	}
+}
+
+// Facebook
+function facebook(){
+    if( (navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPod") != -1) || (navigator.platform.indexOf("iPad") != -1) || navigator.platform.indexOf("Android") != -1) {
+         window.open("https://m.facebook.com/people/Niklas-Mei%C3%9Fner/100004537065246", '_blank');
+	} else {
+		window.open('https://www.facebook.com/people/Niklas-Mei%C3%9Fner/100004537065246', '_blank');
+	}
+}
 
 /* ----- Minimize Nav ----- */
 function minimize() {
